7856e4eb7487c7315c23de50ad868991a6538474
jaiconM <jaicon.marmitt@gmail.com>
2022-09-10 16:59:54 -0300

    adicionando projeto com docker para authorize

---
+++ PushPlayAuthorize.API/Controllers/AlbumController.cs
@@ -0,0 +1,55 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using PushPlay.Application.AlbumContext.Dto;
+using PushPlay.Application.AlbumContext.Handler.Command;
+using PushPlay.Application.AlbumContext.Handler.Query;
+
+namespace PushPlayAuthorize.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlbumController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AlbumController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListarTodos()
+        {
+            var resut = await _mediator.Send(new GetAllAlbumQuery());
+            return Ok(resut.Albums);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar(AlbumInputDto dto)
+        {
+            var result = await _mediator.Send(new CreateAlbumCommand(dto));
+            return Created($"{result.Album.Id}", result.Album);
+        }
+
+        [HttpGet("ListarPorId/{id}")]
+        public async Task<IActionResult> ListarPorId(Guid id)
+        {
+            var result = await _mediator.Send(new GetByIdAlbumQuery(id));
+            return Ok(result.Album);
+        }
+
+        [HttpPut("Atualizar/{id}")]
+        public async Task<IActionResult> Atualizar(Guid id, AlbumInputDto dto)
+        {
+            var result = await _mediator.Send(new UpdateAlbumCommand(id, dto));
+            return Ok(result.Album);
+        }
+
+        [HttpDelete("Excluir/{id}")]
+        public async Task<IActionResult> Excluir(Guid id)
+        {
+            await _mediator.Send(new DeleteAlbumCommand(id));
+            return Ok("Exclusão realizada com sucesso!");
+        }
+    }
+}
---
+++ PushPlayAuthorize.API/Program.cs
@@ -0,0 +1,28 @@
+using PushPlay.Application.Config;
+using PushPlay.Data.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services
+    .RegisterApplication()
+    .RegisterRepository(builder.Configuration.GetConnectionString("PushPlayDBConn"));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
